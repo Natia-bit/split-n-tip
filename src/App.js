40ccb98c45a8e737ad6154505e712c693c57f89c
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-03-26 10:38:34 +0200

    basic bill layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
-function App() {
-  return <div className="App">Hello World</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <MainTitle>Split & Tip</MainTitle>
+      <Bill></Bill>
+    </div>
+  );
 }
 
-export default App;
+function MainTitle({ children }) {
+  return <h1>{children}</h1>;
+}
+
+function Title({ children }) {
+  return <h2>{children}</h2>;
+}
+
+function Bill() {
+  return (
+    <div className="bill">
+      <Title>Bill</Title>
+      <label>Bill amount</label>
+      <input type="text"></input>
+
+      <p className="person-bill-output">
+        Final bill <span>R XX</span>{" "}
+      </p>
+      <p>Final payment details including tips </p>
+      <PersonBillOutputText name="Dude One"></PersonBillOutputText>
+      <PersonBillOutputText name="Dude Two"></PersonBillOutputText>
+    </div>
+  );
+}
+
+function PersonBillOutputText({ name }) {
+  return (
+    <div className="person-bill-output">
+      {name} <span>R XX</span>
+    </div>
+  );
+}
